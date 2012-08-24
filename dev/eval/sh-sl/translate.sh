75560603ca93fb72a0018d7a5e9b7fd230e7a024
Hrvoje Peradin <hperadin@gmail.com>
2012-08-24 13:01:00 +0000

    typo

--- dev/eval/sh-sl/translate.sh
+++ dev/eval/sh-sl/translate.sh
@@ -1,11 +1,11 @@
 # Generate the translation
-cat ../story1_sh.txt | apertium -d ../../../ sh-sl > story1.sh-sl.txt
+cat ../story1_hr.txt | apertium -d ../../../ sh-sl > story1.sh-sl.txt
 
 # Generate the analyses, chunker, interchunk and postchunk
-cat ../story1_sh.txt | apertium -d ../../../ sh-sl-morph > story1.sh-sl-morph.txt
-cat ../story1_sh.txt | apertium -d ../../../ sh-sl-chunker > story1.sh-sl-chunker.txt
-cat ../story1_sh.txt | apertium -d ../../../ sh-sl-interchunk > story1.sh-sl-interchunk.txt
-cat ../story1_sh.txt | apertium -d ../../../ sh-sl-postchunk > story1.sh-sl-postchunk.txt
+cat ../story1_hr.txt | apertium -d ../../../ sh-sl-morph > story1.sh-sl-morph.txt
+cat ../story1_hr.txt | apertium -d ../../../ sh-sl-chunker > story1.sh-sl-chunker.txt
+cat ../story1_hr.txt | apertium -d ../../../ sh-sl-interchunk > story1.sh-sl-interchunk.txt
+cat ../story1_hr.txt | apertium -d ../../../ sh-sl-postchunk > story1.sh-sl-postchunk.txt
 
 # Generate the CG output
-cat ../story1_sh.txt | apertium -d ../../../ sh-sl-constraints > story1.sh-sl-constraints.txt
+cat ../story1_hr.txt | apertium -d ../../../ sh-sl-constraints > story1.sh-sl-constraints.txt
